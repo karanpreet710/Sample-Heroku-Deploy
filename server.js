b7cc3b224faa8a259d7b3c12dbee41f4782ab2b2
Karanpreet Singh <singhkaranpreet710@gmail.com>
2022-10-23 23:27:08 +0530

    Added priority
    Signed-off-by: Karanpreet Singh <singhkaranpreet710@gmail.com>

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.post('/tasks',async (req,res)=>{
     res.send(await Tasks.create(req.body))
 })
 
-db.sync()
+db.sync({alter:true})
   .then(()=>{
     app.listen(PORT,()=>{
         console.log(`Server started on http://localhost:${PORT}`)

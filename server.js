15b2a8159b1f888815519fd5f610a5adb52478bc
Karanpreet Singh <singhkaranpreet710@gmail.com>
2022-10-23 21:58:08 +0530

    use sqlite db for tasks
    Signed-off-by: Karanpreet Singh <singhkaranpreet710@gmail.com>

--- server.js
+++ server.js
@@ -1,13 +1,28 @@
-const { application } = require('express')
 const express = require('express')
+
+const {db,Tasks} = require('./db')
+
 const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
 
 const PORT = process.env.PORT || 4444
 
-app.get('/',(req,res)=>{
-    res.send('Hello World')
+app.get('/',async (req,res)=>{
+    res.send(await Tasks.findAll())
 })
 
-app.listen(PORT,()=>{
-    console.log(`Server started on http://localhost:${PORT}`)
+app.post('/',async (req,res)=>{
+    res.send(await Tasks.create(req.body))
 })
+
+db.sync()
+  .then(()=>{
+    app.listen(PORT,()=>{
+        console.log(`Server started on http://localhost:${PORT}`)
+    })
+  })
+  .catch(()=>{
+    console.error(new Error('Error connecting to database'))
+  })

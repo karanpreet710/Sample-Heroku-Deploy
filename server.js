b022bf3498912eee30e5507a08e3ad6cb50eb9e0
Karanpreet Singh <singhkaranpreet710@gmail.com>
2022-10-23 20:20:20 +0530

    hello world heroku server
    Signed-off-by: Karanpreet Singh <singhkaranpreet710@gmail.com>

---
+++ server.js
@@ -0,0 +1,11 @@
+const { application } = require('express')
+const express = require('express')
+const app = express()
+
+app.get('/',(req,res)=>{
+    res.send('Hello World')
+})
+
+app.listen(4444,()=>{
+    console.log("Server started on http://localhost:4444")
+})
